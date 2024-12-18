8b8320f90ea2c6554b84a64fb2a6f396f76e9cd6
fatimaKhalidNGI <fatima.khalid@nextgeni.com>
2024-12-18 10:18:57 +0500

    fixed schema

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -1,6 +1,8 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable('users', {
     id : serial('id').primaryKey(),
-    user_email : text('user_email').unique()
+    user_email : text('user_email').unique(),
+    createdAt : timestamp('createdAt').defaultNow().notNull(),
+    updatedAt : timestamp('updatedAt').defaultNow().notNull()
 });
